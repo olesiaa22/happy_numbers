0b6ce3e11932fa98544072a8f346a6ca261cdaef
olesiaa22 <alesyashpotak13@gmail.com>
2024-02-28 12:21:43 +0200

    Initial commit

---
+++ щасливі числа/GenerateChangeNumbersFourTimes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+public class GenerateChangeNumbersFourTimes
+{
+    public static Tuple<int, BigInteger> GenerateNumber()
+    {
+        List<BigInteger> numbers = new List<BigInteger>();
+        int position = GenerateNumbersHelper(new StringBuilder(), 4, numbers);
+        return Tuple.Create(position, numbers[numbers.Count - 1]);
+    }
+
+    private static int GenerateNumbersHelper(StringBuilder currentNumber, int requiredChanges, List<BigInteger> numbers)
+    {
+        int position = -1;
+
+        if (currentNumber.Length > 0)
+        {
+            int changes = CountChanges(currentNumber.ToString());
+            if (changes == requiredChanges)
+            {
+                numbers.Add(BigInteger.Parse(currentNumber.ToString()));
+                position = numbers.Count;
+            }
+        }
+
+        if (currentNumber.Length < 20) // Adjust the length limit as needed
+        {
+            currentNumber.Append("4");
+            int newPosition = GenerateNumbersHelper(currentNumber, requiredChanges, numbers);
+            if (newPosition != -1)
+                position = newPosition;
+            currentNumber.Length--; // Backtrack
+
+            currentNumber.Append("7");
+            newPosition = GenerateNumbersHelper(currentNumber, requiredChanges, numbers);
+            if (newPosition != -1)
+                position = newPosition;
+            currentNumber.Length--; // Backtrack
+        }
+
+        return position;
+    }
+
+    private static int CountChanges(string number)
+    {
+        int changes = 0;
+        for (int i = 1; i < number.Length; i++)
+        {
+            if (number[i] != number[i - 1])
+            {
+                changes++;
+            }
+        }
+        return changes;
+    }
+}
---
+++ щасливі числа/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вітаю! Ця програма знаходить позицію щасливого числа і виводить її на екран");
+        Console.WriteLine("Якщо ви хочете побачити щасливе число, яке буде мати змінну чисел 4 рази, введіть число 1");
+        Console.WriteLine("Якщо ви хочете побачити щасливе число, яке буде мати змінну чисел 7 разів, введіть число 2");
+        Console.WriteLine("Якщо хочете одразу перейти до відповіді, введіть число 3");
+        Console.WriteLine("Якщо хочете завершити програму, введіть число 0");
+
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        var resultFourTimes = GenerateChangeNumbersFourTimes.GenerateNumber();
+                        Console.WriteLine($"Щасливе число яке змінюється чотири рази: {resultFourTimes.Item2}, Позиція: {resultFourTimes.Item1}");
+                        break;
+                    case 2:
+                        var resultSevenTimes = GenerateChangeNumbersSevenTimes.GenerateNumber();
+                        Console.WriteLine($"Щасливе число яке змінюється сім разів: {resultSevenTimes.Item2}, Позиція: {resultSevenTimes.Item1}");
+                        break;
+                    case 3:
+                        var resultLuckySevenTimes = GenerateChangeNumbersSevenTimes.GenerateNumber();
+                        var position = GenerateChangeNumbersSevenTimes.GenerateNumber();
+                        Console.WriteLine($"Найщасливіше число: {resultLuckySevenTimes.Item2}, Позиція: {position.Item1}");
+                        break;
+                    case 0:
+                        Console.WriteLine("Програма завершила свою роботу!");
+                        return; 
+                    default:
+                        Console.WriteLine("Невірний вибір. Будь ласка, введіть правильне число.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неправильний формат даних. Будь ласка, введіть число.");
+            }
+        }
+    }
+}
